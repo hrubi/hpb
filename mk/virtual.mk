a69cc42a7a65537eaa999a43b1b370218a5f8270
Jan Hruban <hrubi@hrubi.cz>
2013-02-07 10:04:03 +0100

    add virtual.mk and toolchain recepie stubs

---
+++ mk/virtual.mk
@@ -0,0 +1,21 @@
+# Makefile for virtual packages.
+# These are usually used to pull in dependencies.
+# However, they're free to redefine back any build step
+
+define fetch
+endef
+
+define extract
+endef
+
+define patch
+endef
+
+define config
+endef
+
+define build
+endef
+
+define dest
+endef
